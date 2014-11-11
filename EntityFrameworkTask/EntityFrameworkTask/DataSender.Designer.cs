7fc78685685f03629b5ea782e8df8c348e5547e8
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-11 19:35:20 +0500

    Предыдущий коммит был ошибочным. Добавила настоящий.

---
+++ EntityFrameworkTask/EntityFrameworkTask/DataSender.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace EntityFrameworkTask
+{
+    partial class DataSender
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.groupNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.students = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.universitiesCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(122, 19);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(169, 20);
+            this.firstNameTextBox.TabIndex = 0;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(122, 51);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(169, 20);
+            this.lastNameTextBox.TabIndex = 1;
+            // 
+            // groupNameTextBox
+            // 
+            this.groupNameTextBox.Location = new System.Drawing.Point(122, 83);
+            this.groupNameTextBox.Name = "groupNameTextBox";
+            this.groupNameTextBox.Size = new System.Drawing.Size(169, 20);
+            this.groupNameTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Группа";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Университет";
+            // 
+            // students
+            // 
+            this.students.Location = new System.Drawing.Point(55, 283);
+            this.students.Name = "students";
+            this.students.Size = new System.Drawing.Size(283, 96);
+            this.students.TabIndex = 7;
+            this.students.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(154, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Студенты";
+            // 
+            // universitiesCheckedListBox
+            // 
+            this.universitiesCheckedListBox.FormattingEnabled = true;
+            this.universitiesCheckedListBox.Location = new System.Drawing.Point(122, 109);
+            this.universitiesCheckedListBox.Name = "universitiesCheckedListBox";
+            this.universitiesCheckedListBox.Size = new System.Drawing.Size(169, 94);
+            this.universitiesCheckedListBox.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(122, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 24);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Добавить студента";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // DataSender
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(387, 415);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.universitiesCheckedListBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.students);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupNameTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Name = "DataSender";
+            this.Text = "DataSender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox groupNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox students;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckedListBox universitiesCheckedListBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EntityFrameworkTask/EntityFrameworkTask/DataSender.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EntityFrameworkTask.Model;
+
+namespace EntityFrameworkTask
+{
+    public partial class DataSender : Form
+    {
+        private GuestBookEntities guestBook;
+        public DataSender()
+        {
+            InitializeComponent();
+            guestBook = new GuestBookEntities();
+            AddUniversities();
+            UpdateStudents();
+        }
+
+        private void AddUniversities()
+        {
+            var universities = guestBook.universities.ToList();
+            foreach (var university in universities)
+            {
+                universitiesCheckedListBox.Items.Add(string.Format("{0}:{1}", university.id, university.name));
+            }
+        }
+
+        private void UpdateStudents()
+        {
+            students.Text = "";
+            var studentsRichBox = guestBook.students.ToList();
+            foreach (var student in studentsRichBox)
+            {
+                students.AppendText(string.Format("{0} {1} {2} {3}\n", student.first_name, student.last_name, student.name_group, student.university.name));
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (firstNameTextBox.Text == "")
+            {
+                MessageBox.Show("Введите имя, пожалуйста!");
+                return;
+            }
+
+            if (lastNameTextBox.Text == "")
+            {
+                MessageBox.Show("Введите фамилию, пожалуйста!");
+                return;
+            }
+
+            if (groupNameTextBox.Text == "")
+            {
+                MessageBox.Show("Введите группу, пожалуйста!");
+                return;
+            }
+
+            if (universitiesCheckedListBox.CheckedItems.Count != 1)
+            {
+                MessageBox.Show("Выберете 1 университет, пожалуйста!");
+                return;
+            }
+            try
+            {
+                var idUniversity = int.Parse(universitiesCheckedListBox.CheckedItems[0].ToString().Split(':').First());
+
+                guestBook.students.Add(new student()
+                {
+                    id = guestBook.students.Count() + 1,
+                    first_name = firstNameTextBox.Text,
+                    last_name = lastNameTextBox.Text,
+                    name_group = groupNameTextBox.Text,
+                    id_university = idUniversity
+                });
+                
+                guestBook.SaveChanges();
+                UpdateStudents();
+                
+                MessageBox.Show("Студент успешно добавлен!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Студент по каким-то техническим причинам не добавлен добавлен!");
+            }
+        }
+    }
+}
